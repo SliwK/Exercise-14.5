9fe4bf00c036f1a75678b2a260f771eb9e48f3d9
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-28 23:42:57 +0100

    Add contact functionality - failure

--- js/App.js
+++ js/App.js
@@ -1,5 +1,30 @@
 // app.js
 var App = React.createClass({
+
+
+  getInitialState(){
+      return{
+        contacts: contacts,
+      };
+  },
+/*  //przenioslem to do contactForm
+
+  addContact(event){
+    event.preventDefault();
+    let firstName = this.refs.firstName.value;
+    let lastName = this.refs.lastName.vaue;
+    let email = this.refs.email.value;
+    let id = Math.floor((Math.random()*100)+1);
+
+    console.log(this.refs.firstName.value);
+
+    this.setState({
+      contacts: [...this.state.contacts, {id, firstName, lastName, email}]
+    });
+  },
+*/
+
+
   render: function() {
     return (
       React.createElement('div', {className: 'app'},
--- js/ContactForm.js
+++ js/ContactForm.js
@@ -1,26 +1,74 @@
 var ContactForm = React.createClass({
   propTypes: {
-    contact: React.PropTypes.object.isRequired
+    contact: React.PropTypes.object.isRequired,
+//    addContact: React.PropTypes.func.isRequired,
   },
+
+  getInitialState () {
+    return {
+      addContact: this.addContact,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email
+    };
+  },
+
+  changeFirstName (event) {
+    this.setState({firstName: event.target.value});
+  },
+
+  changeLastName (event) {
+    this.setState({lastName: event.target.value});
+  },
+
+  changeEmail (event) {
+    this.setState({email: event.target.value});
+  },
+
+//dodałem tutaj bo tu przynajmniej widzę w logach że coś dziła... :(
+  addContact(event){
+    event.preventDefault();
+    let firstName = this.refs.firstName.value;
+    let lastName = this.refs.lastName.vaue;
+    let email = this.refs.email.value;
+    let id = Math.floor((Math.random()*100)+1);
+
+    console.log(this);
+
+    this.setState({
+      contacts: this.state.contacts.concat({id, firstName, lastName, email})
+    });
+
+    this.refs.firstName.value = "";
+    this.refs.lastName.vaue = "";
+    this.refs.email.value = "";
+  },
+
 
   render: function() {
     return (
       React.createElement('div', {className: 'form-elements'},
-          React.createElement('form', {className: 'row contactForm'},
+          React.createElement('form', {onSubmit: this.addContact, className: 'row contactForm'},
             React.createElement('input', {
               type: 'text',
               placeholder: 'Imię',
-              value: this.props.contact.firstName,
+              ref: "firstName",
+              value: this.state.firstName,
+              onChange: this.changeFirstName,
             }),
             React.createElement('input', {
               type: 'text',
               placeholder: 'Nazwisko',
-              value: this.props.contact.lastName,
+              ref: "lastName",
+              value: this.state.lastName,
+              onChange: this.changeLastName,
             }),
             React.createElement('input', {
               type: 'email',
               placeholder: 'Email',
-              value: this.props.contact.email,
+              ref: "email",
+              value: this.state.email,
+              onChange: this.changeEmail,
             }),
             React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
           )
